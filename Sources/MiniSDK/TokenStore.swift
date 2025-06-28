2290d376b90c13b16027bd172abbdfde56ff321d
Onur Yilmaz <ozelonuryilmaz@hotmail.com>
2025-06-29 00:11:35 +0300

    feat: Define TokenStore interface and core functions

--- Sources/MiniSDK/TokenStore.swift
+++ Sources/MiniSDK/TokenStore.swift
@@ -1,10 +1,20 @@
 public protocol TokenStoreProtocol {
-    
+    func saveToken(_ token: String)
+    func retrieveToken() -> String?
 }
 
 public struct TokenStore: TokenStoreProtocol {
+    private let base64Enabled: Bool
     
     public init(base64Enabled: Bool) {
+        self.base64Enabled = base64Enabled
+    }
+    
+    public func saveToken(_ token: String) {
+        
+    }
+    
+    public func retrieveToken() -> String? {
         
     }
 }
