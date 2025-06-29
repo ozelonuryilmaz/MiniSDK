add53196b9918b66a8b64c9099f56bd98c783658
Onur Yilmaz <ozelonuryilmaz@hotmail.com>
2025-06-29 11:27:37 +0300

    chore: Update Package.swift file with latest configuration

--- Package.swift
+++ Package.swift
@@ -1,6 +1,3 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -9,21 +6,21 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MiniSDK",
-            targets: ["MiniSDK"]),
+            targets: ["MiniSDK"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "MiniSDK",
-            dependencies: []
+            dependencies: [],
+            path: "Sources/MiniSDK"
         ),
         .testTarget(
             name: "MiniSDKTests",
-            dependencies: ["MiniSDK"]
+            dependencies: ["MiniSDK"],
+            path: "Tests/MiniSDKTests"
         ),
     ]
 )
